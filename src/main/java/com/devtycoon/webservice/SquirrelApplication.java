ce64b5925b0742bdd8e24fcf656dbc44348bbba2
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-04-14 17:36:31 +0900

    Upadte error

--- src/main/java/com/devtycoon/webservice/SquirrelApplication.java
+++ src/main/java/com/devtycoon/webservice/SquirrelApplication.java
@@ -1,20 +1,19 @@
 package com.devtycoon.webservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.builder.SpringApplicationBuilder;
+        import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class SquirrelApplication {
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "/app/config/springboot-webservice/real-application.yml";
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/Squirrel/real-application.yml";
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(SquirrelApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SquirrelApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 }
