9da4a276e41cd40b84882fb672dad4271e55f115
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-04-14 18:35:12 +0900

    Update application.yml : profile setup

--- src/main/java/com/devtycoon/webservice/SquirrelApplication.java
+++ src/main/java/com/devtycoon/webservice/SquirrelApplication.java
@@ -1,19 +1,13 @@
 package com.devtycoon.webservice;
 
+        import org.springframework.boot.SpringApplication;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.boot.builder.SpringApplicationBuilder;
         import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class SquirrelApplication {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "/app/config/Squirrel/real-application.yml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(SquirrelApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(SquirrelApplication.class, args);
     }
 }
